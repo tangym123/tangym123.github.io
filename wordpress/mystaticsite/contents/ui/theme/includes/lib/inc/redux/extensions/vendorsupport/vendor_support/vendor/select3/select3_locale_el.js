782076d521878b18c588e7e39b2a55cea2fdd7d1
tangym123 <1974895947@qq.com>
2018-09-11 13:18:56 +0800

    更改路径

--- wordpress/mystaticsite/contents/ui/theme/includes/lib/inc/redux/extensions/vendorsupport/vendor_support/vendor/select3/select3_locale_el.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /wordpress/contents/ui/theme\includes\lib\inc\redux\extensions\vendorsupport\vendor_support\vendor\select3\select3_locale_el.js was not found on this server.</p>
-</body></html>
